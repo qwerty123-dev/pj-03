d1af2384858e3f272d38fe62ec61e68d39096e00
User1 <ggt7ij@internet.ru>
2025-03-12 18:32:55 +1200

    Add MAIN func

--- main.cpp
+++ main.cpp
@@ -112,3 +112,108 @@ private:
     std::vector<Message> messages;
     std::string currentUser;
 };
+
+
+int main()
+{
+    Chat chat;
+    std::string username, password, name;
+    int option;
+
+    try
+    {
+        while (true)
+        {
+            if(chat.isUserLoggedIn())
+            {
+                std::cout << "\nMenu: \n";
+                std::cout << "1. Logout\n"; 
+                std::cout << "2. Send Message\n"; 
+                std::cout << "3. Show Messages\n"; 
+                std::cout << "4. Exit\n";
+            }
+            else
+            {
+                std::cout << "\nMenu: \n";
+                std::cout << "1. Register\n";
+                std::cout << "2. Login\n";
+                std::cout << "3. Exit\n";
+            }
+            std::cout << "Choose an option: ";
+            std::cin >> option;
+
+            if (option == 1)
+            {
+                if (!chat.isUserLoggedIn())
+                {
+                    std::cout << "Enter username: ";
+                    std::cin >> username;
+                    std::cout << "Enter password: ";
+                    std::cin >> password;
+                    std::cout << "Enter your name: ";
+                    std::cin >> name;
+
+                    chat.registerUser(username, password, name);
+                    std::cout << "User registered successfully!" << std::endl;
+                }
+                else
+                {
+                    chat.logoutUser();
+                }
+            }
+            else if (option == 2)
+            {
+                if (!chat.isUserLoggedIn())
+                {
+                    std::cout << "Enter username to login: ";
+                    std::cin >> username;
+                    std::cout << "Enter password: ";
+                    std::cin >> password;
+
+                    chat.loginUser(username, password);
+                }
+                else
+                {
+                    std::string receiver, message;
+                    std::cout << "Enter recipient username: ";
+                    std::cin >> receiver;
+                    std::cout << "Enter message: ";
+                    std::cin.ignore();
+                    std::getline(std::cin, message);
+
+                    chat.sendMessage(receiver, message);
+                }
+            }
+            else if (option == 3)
+            {
+                if (!chat.isUserLoggedIn())
+                {
+                    std::cout << "Exiting program." << std::endl;
+                    break;
+                }
+                else
+                {
+                    chat.showMessages();
+                }
+            }
+            else if (option == 4 && chat.isUserLoggedIn())
+            {
+                std::cout << "Exiting program." << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "Invalid option, please try again." << std::endl;
+            }
+            
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+    
+
+}
